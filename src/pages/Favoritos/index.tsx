d467b2830a1d038d823f6b6774ada058702c56be
Renan <renanfigcarneiro@gmail.com>
2021-04-27 20:40:22 -0300

    style: icone para voltar estilizado

---
+++ src/pages/Favoritos/index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import {
+  Star,
+  ChevronLeft,
+  ChevronRight,
+} from '@material-ui/icons';
+import { CircularProgress } from '@material-ui/core';
+import {
+  Container,
+  Avatar,
+  HeaderContainer,
+  HeaderItems,
+  Repos,
+} from './styles';
+import api from '../../services/api';
+
+interface RepositoryParams {
+  user: string;
+}
+
+interface UserRepos {
+  name: string;
+  description: string;
+  html_url: string
+  owner: {
+    avatar_url: string;
+    login: string;
+  }
+
+}
+const Favoritos: React.FC = () => {
+  const [userRepos, setUserRepos] = useState<UserRepos[]>();
+  const { params } = useRouteMatch<RepositoryParams>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api.get(`/users/${params.user}/repos`).then((response) => {
+      setUserRepos(response.data);
+      setLoading(false);
+    });
+  }, [params.user]);
+  return (
+    <Container>
+      {loading
+        ? <CircularProgress size={75} />
+        : userRepos && userRepos.length > 0
+          ? (
+            <>
+              <Link to="/">
+                <ChevronLeft fontSize="large" />
+              </Link>
+              <Avatar src={userRepos[0].owner.avatar_url} />
+              <HeaderContainer>
+                <HeaderItems>
+                  <Link to="/">
+                    Repositórios Favoritos
+                    <Star color="secondary" />
+                  </Link>
+                </HeaderItems>
+                <HeaderItems>
+                  <h3>
+                    User:
+                    {' '}
+                    {userRepos[0].owner.login}
+                  </h3>
+                </HeaderItems>
+              </HeaderContainer>
+              <Repos>
+                {userRepos.map((repo) => (
+                  <a
+                    key={repo.name}
+                    href={repo.html_url}
+                  >
+                    <div>
+                      <strong>{repo.name}</strong>
+                    </div>
+                    <ChevronRight />
+                  </a>
+                ))}
+              </Repos>
+            </>
+          )
+          : <h1>Não foi encontrado repositórios para esta conta</h1> }
+    </Container>
+  );
+};
+
+export default Favoritos;
