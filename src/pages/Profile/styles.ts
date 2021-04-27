06a14893e335d21853bf7b12106748cc7b8d917f
Renan <renanfigcarneiro@gmail.com>
2021-04-27 16:38:57 -0300

    feat: criada pagina de profile

---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  a {
+    align-self: flex-start;
+    transition: transform 0.4s;
+    &:hover {
+      transform: translateX(-10px);
+    }
+  }
+`;
+
+export const Avatar = styled.img`
+  border-radius: 100%;
+  height: 164px;
+  width: 164px;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
+
+`;
+
+export const HeaderItems = styled.div`
+    margin-top: 32px;
+  a {
+    color: #a8a8b3;
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    transition: transform 0.4s;
+    &:hover {
+      transform: translateY(-10px);
+    }
+  }
+  svg {
+    margin-left: 8px !important;
+  }
+  h3 {
+    text-align: center;
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    color: #a8a8b3;
+    margin-left: 16px !important;
+  }
+`;
+
+export const Repos = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.4s;
+    &:hover {
+      transform: translate(10px);
+    }
+    & + a {
+      margin-top: 16px;
+    }
+  }
+  div {
+    margin: 0 16px;
+    flex: 1;
+    strong {
+      font-size: 20px;
+      color: #3d3d4d;
+    }
+    p {
+      font-size: 18px;
+      color: #a8a8b3;
+      margin-top: 4px;
+    }
+  }
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
+`;
