c2aa568aa644e93c73a867da0a441991f5bf41a0
Renan <renanfigcarneiro@gmail.com>
2021-04-27 16:55:49 -0300

    feat: loading adicionado

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -5,6 +5,7 @@ import {
   ChevronLeft,
   ChevronRight,
 } from '@material-ui/icons';
+import { CircularProgress } from '@material-ui/core';
 import {
   Container,
   Avatar,
@@ -31,48 +32,52 @@ interface UserRepos {
 const Profile: React.FC = () => {
   const [userRepos, setUserRepos] = useState<UserRepos[]>();
   const { params } = useRouteMatch<RepositoryParams>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     api.get(`/users/${params.user}/repos`).then((response) => {
       setUserRepos(response.data);
+      setLoading(false);
     });
   }, [params.user]);
   return (
     <Container>
-      { userRepos && userRepos.length > 0
-        ? (
-          <>
-            <Link to="/">
-              <ChevronLeft />
-            </Link>
-            <Avatar src={userRepos[0].owner.avatar_url} />
-            <HeaderContainer>
-              <HeaderItems>
-                <Link to="/">
-                  Repositórios Favoritos
-                  <Star color="secondary" />
-                </Link>
-              </HeaderItems>
-              <HeaderItems>
-                <h3>{userRepos[0].owner.login}</h3>
-              </HeaderItems>
-            </HeaderContainer>
-            <Repos>
-              {userRepos.map((repo) => (
-                <a
-                  key={repo.name}
-                  href={repo.html_url}
-                >
-                  <div>
-                    <strong>{repo.name}</strong>
-                  </div>
-                  <ChevronRight />
-                </a>
-              ))}
-            </Repos>
-          </>
-        )
-        : <h1>Não foi encontrado repositórios para esta conta</h1> }
+      {loading
+        ? <CircularProgress style={{ flex: 1 }} size={75} />
+        : userRepos && userRepos.length > 0
+          ? (
+            <>
+              <Link to="/">
+                <ChevronLeft />
+              </Link>
+              <Avatar src={userRepos[0].owner.avatar_url} />
+              <HeaderContainer>
+                <HeaderItems>
+                  <Link to="/">
+                    Repositórios Favoritos
+                    <Star color="secondary" />
+                  </Link>
+                </HeaderItems>
+                <HeaderItems>
+                  <h3>{userRepos[0].owner.login}</h3>
+                </HeaderItems>
+              </HeaderContainer>
+              <Repos>
+                {userRepos.map((repo) => (
+                  <a
+                    key={repo.name}
+                    href={repo.html_url}
+                  >
+                    <div>
+                      <strong>{repo.name}</strong>
+                    </div>
+                    <ChevronRight />
+                  </a>
+                ))}
+              </Repos>
+            </>
+          )
+          : <h1>Não foi encontrado repositórios para esta conta</h1> }
     </Container>
   );
 };
