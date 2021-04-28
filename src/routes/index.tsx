d09f4e8328f8e342a0aa2c026ea4db9f8b47cad2
Renan <renanfigcarneiro@gmail.com>
2021-04-27 22:39:51 -0300

    fix: ajuste na sintaxe da url

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import Favoritos from '../pages/Favoritos';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/:login+/starred" component={Favoritos} />
     <Route path="/:user+" component={Profile} />
   </Switch>
 );
